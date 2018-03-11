95b64f78f943b351ffcd15a0ffbaa743936f4235
潘卓然 ParnDeedlit <parndeedlits@gmail.com>
2018-03-11 15:23:09 +0800

    添加els接口的示例

--- libs/zondyclient/include-leaflet.js
+++ libs/zondyclient/include-leaflet.js
@@ -87,6 +87,9 @@
     if (inArray(includes, 'animate-marker')) {
        inputScript(httpUrl + "/cdn/leaflet-plugins/Leaflet.AnimatedMarker/src/AnimatedMarker.js");
     }
+    if (inArray(includes, 'realmove-marker')) {
+       inputScript(httpUrl + "/cdn/leaflet-plugins/Leaflet.MovingMarker/MovingMarker.js");
+    }
     if (inArray(includes, 'elasticsearch')) {
        inputScript(httpUrl + "/cdn/elasticsearch/14.1.0/elasticsearch.min.js");
     }
